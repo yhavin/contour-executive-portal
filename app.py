b3faefe5ed10b5dd96258e6f5e86270ede32ec73
yhavin <y.havin@gmail.com>
2026-01-13 13:30:33 -0500

    Add home page basic login functionality

--- app.py
+++ app.py
@@ -1,6 +1,66 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+st.logo("assets/contourcfo_logo.png")
+
+
+def login():
+    st.set_page_config(
+        page_title="Home | Executive Portal Demo",
+        layout="centered"
+    )
+    st.image("assets/contourcfo_logo.png", width=175)
+    st.divider(width=100)
+    st.title(":material/dashboard: Executive Portal Demo")
+
+    if "logged_in" not in st.session_state:
+        password_form_container = st.empty()
+
+        password_form = password_form_container.form(key="password-form", clear_on_submit=True, border=False)
+        password = password_form.text_input(
+            label="Password",
+            placeholder="Enter password to access this demo"
+        )
+
+        submitted = password_form.form_submit_button("Submit")
+        if submitted:
+            try:
+                int(password)
+            except ValueError:
+                st.error("Incorrect password. Please try again.")
+                st.stop()
+
+            if int(password) % 7 == 0:  # Password is any multiple of 7
+                st.session_state["logged_in"] = True
+                st.success("Logged in successfully.")
+                password_form_container.empty()
+                st.rerun()
+            else:
+                st.error("Incorrect password. Please try again.")
+    if "logged_in" in st.session_state and st.session_state["logged_in"]:
+        st.text("Your home for company reports and data insights")
+
+
+login_page = st.Page(
+    page=login,
+    title="Home",
+    icon=":material/home:"
 )
+page2_page = st.Page(
+    page="page2.py",
+    title="Page 2",
+    url_path="page-2",
+    icon=":material/content_copy:"
+)
+
+if "logged_in" in st.session_state and st.session_state["logged_in"]:
+    navigation = {
+        "": [login_page],
+        "Reports": [page2_page]
+    }
+
+    pg = st.navigation(navigation, expanded=True)
+else:
+    pg = st.navigation([login_page])
+
+pg.run()
